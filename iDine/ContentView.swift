6efb7b798e45677141398ab05b75c764dbfcb647
Richard de Borja <richard.deborja@gmail.com>
2019-10-15 22:12:50 -0400

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Richard de Borja on 2019-10-15.
+//  Copyright © 2019 Richard de Borja. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
