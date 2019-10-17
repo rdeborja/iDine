3f6194b2bc14d132a0430fab5bd07c74ed0dc165
Richard de Borja <richard.deborja@gmail.com>
2019-10-17 17:32:47 -0400

    cleaned up and added bi-directional variables

--- iDine/OrderView.swift
+++ iDine/OrderView.swift
@@ -21,18 +21,23 @@ struct OrderView: View {
                             Spacer()
                             Text("$\(item.price)")
                         }
-                    }
+                    }.onDelete(perform: deleteItems)
                 }
                 
                 Section {
-                    NavigationLink(destination: Text("Check out")) {
+                    NavigationLink(destination: CheckoutView()) {
                         Text("Place order")
                     }
-                }
+                }.disabled(order.items.isEmpty)
             }
         .navigationBarTitle("Order")
         .listStyle(GroupedListStyle())
+        .navigationBarItems(trailing: EditButton())
         }
+    }
+    
+    func deleteItems(at offsets: IndexSet) {
+        order.items.remove(atOffsets: offsets)
     }
 }
 
